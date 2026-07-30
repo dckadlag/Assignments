44e0f6e1e85e4fc6fa0c85a0a5abfa1b6d6a4b8c
dckadlag <dckadlag@gmail.com>
2026-07-30 22:37:41 +0300

    Assignment 1- Datatypes

---
+++ typescript-training/Assignments/Assignment1.ts
@@ -0,0 +1,80 @@
+//Assignment:01
+//1. Temperature of a city in degress celsius: 25.5
+let cityTemperature:number=25.5;
+console.log(`Temperature of a city in degress celsius ${cityTemperature}`)
+
+//2. Whether a customer has placed an order: True or false
+let placeOrder:boolean= true;
+console.log (`customer has placed an order ${placeOrder} `)
+
+//3. Persons phone numbers "123-456-7890";
+let phoneNumber:string="123-456-7890";
+console.log(`Persons phone numbers ${phoneNumber}`)
+
+//4: Amount of money in a customers bank account : 1000.50
+let accountBalance:number=1000.50;
+console.log(`Amount of money in a customers bank account ${accountBalance}`);
+
+//5: persons email adress: "john.doe@example.com"
+let emailAdress: string= "john.doe@example.com";
+console.log(`persons email adress ${emailAdress}`);
+
+//6: Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinateOfLocation:number=(37.7749 -122.4194);
+console.log(`Coordinates of a location (latitude, longitude) ${coordinateOfLocation}`);
+
+//7. Person marital status: true or false
+let maritalStatus:boolean= true;
+console.log (`cPerson marital status ${maritalStatus} `)
+
+//8: persons occuption : "Software Engineer"
+let personsOccuption: string= "Software Engineer";
+console.log(`persons occuption ${personsOccuption}`);
+
+//9: Persons faviourite colour: "Blue"
+let faviouriteColour: string= "Blue";
+console.log(`Persons faviourite colour ${faviouriteColour}`);
+
+//10: Current year :2023
+let currentYear: number= 2023;
+console.log(`Current year ${currentYear}`);
+
+//11: Number of followers on a social media platform: 1,000,000
+let numberOfFollowers: number= 1000000
+console.log(`Number of followers on a social media platform ${numberOfFollowers}`);
+
+//12: Rating of a movies:7.5
+let ratingOfMovies: number= 7.5;
+console.log(`Rating of a movies ${ratingOfMovies}`);
+
+//13: Persons Blood type: 'A'
+let bloodType: string= 'A';
+console.log(`Persons Blood type ${bloodType}`);
+
+//14: Title of Book: "To Kill a Mockingbird"
+let titleOfBook: string= "To Kill a Mockingbird";
+console.log(`itle of Book ${titleOfBook}`);
+
+//15: Number of Employee in a Company:500
+let numberOfEmployee: number= 500;
+console.log(`Number of Employee in a Company ${numberOfEmployee}`);
+
+//16: Time of an Event :2:30PM
+let eventTime: string='2:30',PM
+console.log(`Time of an Event ${eventTime}`);
+
+//17: Name of Country: "United states"
+let countryName:string="United states";
+console.log(`Name of Country ${countryName}`)
+
+//18: persons eye clor: "Brown";
+let eyeColor:string="Brown";
+console.log(`persons eye clor ${eyeColor}`)
+
+//19: persons Birthplace: ="New York City"
+let birthPlace:string="New York City";
+console.log(`persons Birthplace ${birthPlace}`)
+
+//Distance between Two cities: 200.5
+let distanceBetweenCities: number= 200.5;
+console.log(`Distance between Two cities ${distanceBetweenCities}`);
--- typescript-training/data-types.ts
+++ typescript-training/data-types.ts
@@ -1,70 +1,71 @@
-//Dta types in java divided in to two different types
-//1. Primitive data types:=> (imutable data types) (original value want change if we are going to perfor oper)
-//2. Non primitive data types (mutable data types) (original value will change if we are going to perfor oper)
+//Typescripts:=> it is the superset of java script: 
+// Syntex to store the data:
+// declearation varaiable: data type=data:
+//in Typescript Two diff data type is there'
 
-//Imutable:
-/*let a=10;
-a+10;
-console.log(a);
-
-//mutable
-let empData={
-    "name" : "Dhananjay",
-    "id" :153
-}
-empData.age= 36;
-console.log(empData);
-*/
 //****************Primitive Data types******************
 //1. Number: => the data can store number with or without decimals
 // number should be stored any quotations
-let num1=12345;
-let num2=10.15;
+let num1: number=12345;
+let num2: number=10.15;
 console.log(typeof num1);
 console.log(typeof num2);
+// num1="Dhananjay";
+// console.log(num1);
 
 //string => the data type than can store text , char and words
 // string should be stored in quatation (single quotes or double quotes or back ticks)
-let firstName="Dhananjay";
-let lastName="Kadlag";
-let role = "'Senior' Executive QA";
-console.log(typeof firstName);
-console.log(typeof lastName);
-console.log(typeof role);
+let firstName: string="Dhananjay";
+let lastName: string="Kadlag";
+let role: string= "'Senior' Executive QA";
+console.log(firstName);
+console.log(lastName);
+console.log( role);
 
 let empInfo =`emp first name is ${firstName}, emp last name is ${lastName}, emp role is ${role}`;
 console.log(empInfo);  //to print dynamic value use backticks and ${} like wise
 
 //3. Boolean => store the result of condition true or false
-let  result=10>20;
-console.log(typeof result);
+let  result:boolean=10>20;
+console.log(result);
 
 //Undefined => represent varaible that has been decleared but not assign any value
-let empId;
+let empId:undefined;
 console.log(typeof empId);
 
 //null:=> null represent varaible that has been decleared and assign a null value or empty value intentionally
-let salary =1000;
+let salary:null=null;
 salary =null;
 console.log(salary);
 
-// symbol:=> Symbol data types represents unique identifier inside the object
-let countryOfOrigin = symbol();
-let productInfo = {
-    "productName": "IPhone",
-    "model": "17ProMax",
-    [countryOfOrigin]: "china"
-}
-console.log("productInfo");
+//union:=>union represent more than one data type within varaiables/combination of multiple data types
+let empAdress:string | number="Sangamner";
+console.log(empAdress);
 
+empAdress=500001; 
+console.log(empAdress);
+
+//any:=>it represents any data types is allowed in varaible(whenever we use any type safety removed)
+let data : any="dhananjay";
+console.log(data);
+data=1234;
+console.log(data);
+data=true
+console.log(data);
 
 //**************************Non Primitive Data types *****************************
 //object:=>it represt a collection of key-value pairs store togather
-/*let empName="Dhananjay";
-let empId= 1234;
-let visaStatus =true;
-*/
-let empData={
+interface empInfo {
+      "empName":string,
+    "empId" :number,
+    "visaStatus" : boolean,
+    "address":{
+        "village": string,
+        "street" : string,
+        "state": string
+}
+}
+let empData:empInfo ={
     "empName":"Dhananjay",
     "empId" :1234,
     "visaStatus" : true,
@@ -79,45 +80,55 @@ let empData={
 console.log(empData.empName);
 console.log(empData.address.village);
 
-//2. Method 1: (dynamic Data)
-console.log(empData["empName"]);
-console.log(empData["address"]["village"]);
-
+//Array: Represent list of values
 // Array:=> it is the data type that can store list of values.
-let fruits = ["apple", "Bananana", "mango", "Grapes"];
-let prices= [200,80,100,120];
-let fruitsAndPrices = ["apple",200, "Bananana", 80, "mango", 100, "Grapes", 120];
+let fruits : string[]= ["apple", "Bananana", "mango", "Grapes"];
+let prices:number[]= [200,80,100,120];
+let fruitsAndPrices: (number | string)[]= ["apple",200, "Bananana", 80, "mango", 100, "Grapes", 120];
 //access the data from array 
 console.log(fruits[0]);
 console.log(prices[1]);
 console.log(fruitsAndPrices[3]);
 
+//Tuples"=>is an order array in type script
+//problem statement: store emp name emp id, visa status within the array
+//array
+let empDetailss:(string|number|boolean)[]=[1234, "Dhananjay", true];
+
+//Tuples
+let empDetails:[string, number, boolean]=["Dhananjay", 1234, true];
+
+
 //Functions:=> function is the block of code that can be executed when it is called
+
 //Steps to create the Function:
 //1. Identified the duplicate code and seperate from the program
 //2.seperate the duplicate code and add inside the curly braces/block
 //3. Add a unique name to each and every block to call the function , along with function
 
-function loginIntoApplication(browserName){
+function loginIntoApplication(browserName: string, url: string): void{
 console.log(`launch the ${browserName} Browser`);
-console.log("enter the url: https://www.icici.com/");
+console.log("enter the url: ${url}");
 console.log("Enter the username as 'Dhananjay' and password as 'Mangal@123'");
 console.log("click on the login button");
 }
 
-function logoutFromApplication() {
+function logoutFromApplication(): void {
 console.log("logout from the application");
 console.log("close the brouser");
 }
 
-function getAccountBalance(){
+function getAccountBalance():number{
 console.log("navigate to the account balance page");
 let accountBalance = 100000;//block scope varaibale
 return accountBalance; // the value will assign to function name
 }
+function fail():never{
+throw new Error("this function will never return any value");
+}
 
 //set:=> set is the datatype which represent collection of data types/ duplicate not allowed
-let empIds = new Set();
+let empIds: Set<number>= new Set();
 empIds.add(1234);
 empIds.add(123456);
 empIds.add(1234);
@@ -126,32 +137,8 @@ empIds.add(12345678);
 console.log(empIds)
 
 //map:=> it represent collection of Key- value paire and key should be unique
-let empDataa = new Map();
+let empDataa: Map<number,string> = new Map();
 empDataa.set(1234, "Dhananjay");
 empDataa.set(1234, "Dhanajay");
 empDataa.set(123, "raghav");
 console.log(empDataa);
-
-//date:=>date represent date and time
-let currentDate = new Date();
-console.log(currentDate);
-//get the current date
-console.log(currentDate.getDate());
-//current month
-console.log(currentDate.getMonth());
-//current year
-console.log(currentDate.getFullYear());
-//current hour
-console.log(currentDate.getHours());
-//current MInutes
-console.log(currentDate.getMinutes());
-//current seconds
-console.log(currentDate.getSeconds());
-//current milisecond
-console.log(currentDate.getMilliseconds());
-
-//current day
-console.log(currentDate.getDay()+1);
-
-let email = `dhananjaykadlag${currentDate.getTime()} @gmail.com`;
-console.log(email);
